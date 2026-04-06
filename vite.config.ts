c0527f806e79d9e5daaaa0eb8076db066bd543fb
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-04-06 23:38:25 +0300

    fix: include .d.ts files

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,16 @@ import { resolve } from 'node:path';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
+import dts from 'vite-plugin-dts';
 import solid from 'vite-plugin-solid';
 
 export default defineConfig({
-    plugins: [solid(), tailwindcss(), checker({ typescript: true })],
+    plugins: [
+        solid(),
+        tailwindcss(),
+        checker({ typescript: true }),
+        dts({ tsconfigPath: './tsconfig.build.json', rollupTypes: true }),
+    ],
     build: {
         lib: {
             entry: resolve(__dirname, 'src/index.ts'),
