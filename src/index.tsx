12f9222ee4f8e82660f661fbe3f261cb29cafe1d
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-04-06 22:19:18 +0300

    setup vite

---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import type { VoidComponent } from 'solid-js';
+
+export const TestComponent: VoidComponent = () => {
+    return <div>Test Component</div>;
+};
---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
+import solid from 'vite-plugin-solid';
+
+export default defineConfig({
+    plugins: [solid(), tailwindcss(), checker({ typescript: true })],
+});
