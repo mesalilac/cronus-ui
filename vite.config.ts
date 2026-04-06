4d311affa528930e0ecf1755215317de88659b4a
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-04-07 00:25:18 +0300

    update entery files

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
     build: {
         lib: {
             entry: resolve(__dirname, 'src/index.ts'),
-            name: 'CronusUI',
+            name: 'cronus-ui',
             formats: ['es'],
         },
         rolldownOptions: {
