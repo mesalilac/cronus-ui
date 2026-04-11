a78ca768e3ff76c4831c2dc8020a276186f598af
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-04-11 14:04:47 +0300

    refactor(ui/Textarea): use `Boolean` for `aria-invalid`

--- src/ui/Textarea/Textarea.tsx
+++ src/ui/Textarea/Textarea.tsx
@@ -118,7 +118,7 @@ export const Textarea = <T = string>(props: TextareaProps<T>) => {
             </Show>
             <textarea
                 aria-describedby={error() ? `${id}-error` : `${id}-helper`}
-                aria-invalid={!!error()}
+                aria-invalid={Boolean(error())}
                 class={cn(
                     'h-32 max-h-32 resize-none rounded-lg border border-neutral-600 bg-neutral-700/30 px-3 py-2.5 text-sm placeholder:text-neutral-500 focus:outline-none focus:ring-2 focus:ring-blue-500',
                     error() && 'bg-red-500/30 focus:ring-red-500',
