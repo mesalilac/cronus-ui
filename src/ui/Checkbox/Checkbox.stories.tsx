853053e1146e7f0ced599d25ae79698963c60d09
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-04-11 13:25:24 +0300

    storybook: add `Checkbox`

---
+++ src/ui/Checkbox/Checkbox.stories.tsx
@@ -0,0 +1,54 @@
+import { createSignal } from 'solid-js';
+import type { Meta, StoryObj } from 'storybook-solidjs-vite';
+
+import { Checkbox } from './Checkbox';
+
+type Component = typeof Checkbox;
+
+const meta = {
+    title: 'Checkbox',
+    component: Checkbox,
+    parameters: {
+        layout: 'centered',
+    },
+} satisfies Meta<Component>;
+
+export default meta;
+type Story = StoryObj<Component>;
+
+export const Default: Story = {
+    render: () => {
+        const [checked, setChecked] = createSignal(false);
+
+        return <Checkbox checked={checked()} onChange={setChecked} />;
+    },
+};
+
+export const WithLabel: Story = {
+    render: () => {
+        const [checked, setChecked] = createSignal(false);
+
+        return (
+            <Checkbox
+                checked={checked()}
+                label='Checkbox Label'
+                onChange={setChecked}
+            />
+        );
+    },
+};
+
+export const WithHelperText: Story = {
+    render: () => {
+        const [checked, setChecked] = createSignal(false);
+
+        return (
+            <Checkbox
+                checked={checked()}
+                helper='Helper Text'
+                label='Checkbox Label'
+                onChange={setChecked}
+            />
+        );
+    },
+};
