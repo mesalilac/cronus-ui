779063c75bc381d7710c6d2cdeb76c3a29e13b14
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-04-11 13:11:17 +0300

    storybook: add story for `Badge`

---
+++ src/ui/Badge/Badge.stories.tsx
@@ -0,0 +1,51 @@
+import type { Meta, StoryObj } from 'storybook-solidjs-vite';
+
+import { Badge } from './Badge';
+
+type Component = typeof Badge;
+
+const meta = {
+    title: 'Badge',
+    component: Badge,
+    parameters: {
+        layout: 'centered',
+    },
+} satisfies Meta<Component>;
+
+export default meta;
+type Story = StoryObj<Component>;
+
+export const Default: Story = {
+    args: {
+        variant: 'default',
+        children: 'Badge Label',
+    },
+};
+
+export const Success: Story = {
+    args: {
+        variant: 'success',
+        children: 'Badge Label',
+    },
+};
+
+export const Warning: Story = {
+    args: {
+        variant: 'warning',
+        children: 'Badge Label',
+    },
+};
+
+export const Danger: Story = {
+    args: {
+        variant: 'danger',
+        children: 'Badge Label',
+    },
+};
+
+export const Outline: Story = {
+    args: {
+        variant: 'outline',
+        children: 'Badge Label',
+    },
+};
