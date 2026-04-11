264ed93d417db4d2467b9eb678f3a49b18e2b924
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-04-11 13:34:32 +0300

    storybook: add `ToggleSwitch`

---
+++ src/ui/ToggleSwitch/ToggleSwitch.stories.tsx
@@ -0,0 +1,70 @@
+import { createSignal } from 'solid-js';
+import type { Meta, StoryObj } from 'storybook-solidjs-vite';
+
+import { ToggleSwitch } from './ToggleSwitch';
+
+type Component = typeof ToggleSwitch;
+
+const meta = {
+    title: 'ToggleSwitch',
+    component: ToggleSwitch,
+    parameters: {
+        layout: 'centered',
+    },
+} satisfies Meta<Component>;
+
+export default meta;
+type Story = StoryObj<Component>;
+
+export const Default: Story = {
+    render: () => {
+        const [checked, setChecked] = createSignal(false);
+
+        return <ToggleSwitch checked={checked()} onChange={setChecked} />;
+    },
+};
+
+export const WithLabel: Story = {
+    render: () => {
+        const [checked, setChecked] = createSignal(false);
+
+        return (
+            <ToggleSwitch
+                checked={checked()}
+                label='Checkbox Label'
+                onChange={setChecked}
+            />
+        );
+    },
+};
+
+export const WithHelperText: Story = {
+    render: () => {
+        const [checked, setChecked] = createSignal(false);
+
+        return (
+            <ToggleSwitch
+                checked={checked()}
+                helper='Helper Text'
+                label='Checkbox Label'
+                onChange={setChecked}
+            />
+        );
+    },
+};
+
+export const Disabled: Story = {
+    render: () => {
+        const [checked, setChecked] = createSignal(false);
+
+        return (
+            <ToggleSwitch
+                checked={checked()}
+                disabled
+                helper='Helper Text'
+                label='Checkbox Label'
+                onChange={setChecked}
+            />
+        );
+    },
+};
