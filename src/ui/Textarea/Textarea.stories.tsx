ce70fcf42c1b3202269576b3b91364bd6aa87841
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-04-13 05:52:50 +0300

    storybook: add `Textarea`

---
+++ src/ui/Textarea/Textarea.stories.tsx
@@ -0,0 +1,93 @@
+import { createSignal } from 'solid-js';
+import type { Meta, StoryObj } from 'storybook-solidjs-vite';
+
+import { Textarea } from './Textarea';
+
+const meta = {
+    title: 'Ui/Textarea',
+    component: Textarea,
+    parameters: {
+        layout: 'centered',
+    },
+} satisfies Meta<typeof Textarea>;
+
+export default meta;
+type Story = StoryObj<typeof Textarea>;
+
+export const Default: Story = {
+    render: () => {
+        const [value, setValue] = createSignal('');
+
+        return (
+            <Textarea
+                onInput={(v) => setValue(v)}
+                parse={(raw) => raw}
+                value={value()}
+            />
+        );
+    },
+};
+
+export const WithLabel: Story = {
+    render: () => {
+        const [value, setValue] = createSignal('');
+
+        return (
+            <Textarea
+                label='Label'
+                onInput={(v) => setValue(v)}
+                parse={(raw) => raw}
+                value={value()}
+            />
+        );
+    },
+};
+
+export const WithLabelAndRequired: Story = {
+    render: () => {
+        const [value, setValue] = createSignal('');
+
+        return (
+            <Textarea
+                label='Label'
+                onInput={(v) => setValue(v)}
+                parse={(raw) => raw}
+                required
+                value={value()}
+            />
+        );
+    },
+};
+
+export const WithHelperText: Story = {
+    render: () => {
+        const [value, setValue] = createSignal('');
+
+        return (
+            <Textarea
+                helper='Helper Text'
+                onInput={(v) => setValue(v)}
+                parse={(raw) => raw}
+                value={value()}
+            />
+        );
+    },
+};
+
+export const WithError: Story = {
+    render: () => {
+        const [value, setValue] = createSignal('');
+
+        return (
+            <Textarea
+                onInput={(v) => setValue(v)}
+                parse={(raw) => raw}
+                validate={(v) => {
+                    if (v.length < 10)
+                        return 'Must be at least 10 characters long';
+                }}
+                value={value()}
+            />
+        );
+    },
+};
