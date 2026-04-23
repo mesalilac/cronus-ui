8b2be53a7245acbac01df8cd2984f564331d8db6
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-04-23 15:24:18 +0300

    refactor(ui/Modal): use `dialog` tag

--- src/ui/Modal/Content.tsx
+++ src/ui/Modal/Content.tsx
@@ -1,11 +1,109 @@
-import type { JSX } from 'solid-js';
+import { createEffect, type JSX, onCleanup } from 'solid-js';
+import { Portal } from 'solid-js/web';
+import { Transition } from 'solid-transition-group';
 
+import { useModalContext } from '~/ui/Modal/context';
 import { cn } from '~/utils';
 
 export const Content = (props: { class?: string; children: JSX.Element }) => {
+    let dialogRef!: HTMLDialogElement;
+
+    const ctx = useModalContext();
+
+    const onEnterAnim = (el: Element, done: () => void) => {
+        const overlay = el;
+        const modalWindow = overlay.querySelector('[data-slot="modal-window"]');
+
+        const tl = gsap.timeline({ onComplete: done });
+
+        tl.fromTo(
+            overlay,
+            {
+                autoAlpha: 0,
+            },
+            {
+                autoAlpha: 1,
+                duration: 0.15,
+                ease: 'power1.out',
+            },
+        ).fromTo(
+            modalWindow,
+            {
+                autoAlpha: 0,
+                y: 16,
+                scale: 0.96,
+            },
+            {
+                autoAlpha: 1,
+                y: 0,
+                scale: 1,
+                duration: 0.2,
+                ease: 'power1.out',
+            },
+            '<',
+        );
+    };
+
+    const onExitAnim = (el: Element, done: () => void) => {
+        const overlay = el;
+        const modalWindow = overlay.querySelector('[data-slot="modal-window"]');
+
+        const tl = gsap.timeline({ onComplete: done });
+
+        tl.to(modalWindow, {
+            autoAlpha: 0,
+            scale: 0.96,
+            y: 12,
+            duration: 0.18,
+            ease: 'power1.in',
+        }).to(
+            overlay,
+            {
+                autoAlpha: 0,
+                duration: 0.12,
+                ease: 'power1.in',
+            },
+            '<',
+        );
+    };
+
+    createEffect(() => {
+        if (ctx.isOpen()) {
+            dialogRef.showModal();
+
+            const abortController = new AbortController();
+
+            const originalOverflow = window.getComputedStyle(
+                document.body,
+            ).overflow;
+            document.body.style.overflow = 'hidden';
+
+            document.addEventListener(
+                'keydown',
+                (e: KeyboardEvent) => {
+                    if (e.key === 'Escape') {
+                        ctx.closeModal();
+                    }
+                },
+                abortController,
+            );
+
+            onCleanup(() => {
+                document.body.style.overflow = originalOverflow;
+                abortController.abort();
+            });
+        } else {
+            dialogRef.close();
+        }
+    });
+
     return (
-        <div class={cn('flex flex-col gap-4 overflow-y-auto p-2', props.class)}>
-            {props.children}
-        </div>
+        <Portal>
+            <Transition onEnter={onEnterAnim} onExit={onExitAnim}>
+                <dialog class={cn(props.class)} ref={dialogRef}>
+                    {props.children}
+                </dialog>
+            </Transition>
+        </Portal>
     );
 };
