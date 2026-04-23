549e4ac231aa8eed5bd3c327248977ce5b5b90e5
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-04-24 01:52:35 +0300

    fix(ui/DropdownMenu): missing sub components

--- src/ui/DropdownMenu/DropdownMenu.tsx
+++ src/ui/DropdownMenu/DropdownMenu.tsx
@@ -1,6 +1,13 @@
 import { Content } from '~/ui/DropdownMenu/Content';
 import { Trigger } from '~/ui/DropdownMenu/Trigger';
 import { Menu, type MenuCompound } from '~/ui/Menu';
+import { Item } from '~/ui/Menu/Item';
+import { ItemCheckbox } from '~/ui/Menu/ItemCheckbox';
+import { ItemSwitch } from '~/ui/Menu/ItemSwitch';
+import { Label } from '~/ui/Menu/Label';
+import { RadioGroup } from '~/ui/Menu/RadioGroup';
+import { MenuSeparator } from '~/ui/Menu/Separator';
+import { Sub } from '~/ui/Menu/Sub';
 
 type DropdownMenuCompound = MenuCompound & {
     Trigger: typeof Trigger;
@@ -13,3 +20,11 @@ export const DropdownMenu = ((props) => {
 
 DropdownMenu.Trigger = Trigger;
 DropdownMenu.Content = Content;
+DropdownMenu.Item = Item;
+DropdownMenu.ItemSwitch = ItemSwitch;
+DropdownMenu.ItemCheckbox = ItemCheckbox;
+DropdownMenu.Label = Label;
+DropdownMenu.Separator = MenuSeparator;
+
+DropdownMenu.Sub = Sub;
+DropdownMenu.RadioGroup = RadioGroup;
