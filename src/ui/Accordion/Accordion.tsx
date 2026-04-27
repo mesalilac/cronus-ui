b0b2b19f6c3ddf36e22786f295bba05b31634a60
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-04-27 15:41:46 +0300

    feat(ui/Accordion): init comp

---
+++ src/ui/Accordion/Accordion.tsx
@@ -0,0 +1,7 @@
+import type { Component } from 'solid-js';
+
+export type AccordionProps = {};
+
+export const Accordion: Component<AccordionProps> = (props) => {
+    return <div>Accordion</div>;
+};
