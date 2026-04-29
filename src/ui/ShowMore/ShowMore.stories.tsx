7b275bc629b9726a53f703ebe3c2cff4126833b6
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-04-29 10:59:11 +0300

    feat(ui/ShowMore): write component

---
+++ src/ui/ShowMore/ShowMore.stories.tsx
@@ -0,0 +1,65 @@
+import type { Meta, StoryObj } from 'storybook-solidjs-vite';
+
+import { ShowMore } from './ShowMore';
+
+type StoryComponent = typeof ShowMore;
+
+const meta = {
+    title: 'Ui/ShowMore',
+    component: ShowMore,
+    parameters: {
+        layout: 'centered',
+    },
+} satisfies Meta<StoryComponent>;
+
+export default meta;
+type Story = StoryObj<StoryComponent>;
+
+const LoremIpsum = () => {
+    return (
+        <>
+            Amet adipisicing excepteur id fugiat aliquip ea dolore velit. Culpa
+            sunt ad irure sint officia aliqua labore id deserunt ullamco. Minim
+            irure nostrud sunt do culpa excepteur nulla nulla cupidatat. Dolor
+            adipisicing tempor dolore nisi fugiat. Nostrud Lorem consequat in
+            dolore. Cillum qui eu sint ipsum. Magna commodo nostrud culpa dolor
+            Lorem aliqua culpa. Fugiat do nulla pariatur consequat veniam
+            adipisicing mollit laborum sunt. Ipsum id ullamco tempor ad cillum
+            exercitation tempor in mollit pariatur laborum mollit. Aute fugiat
+            adipisicing consequat tempor exercitation ex sunt et cillum aute
+            consectetur. Proident nulla ut do proident eiusmod dolore dolore.
+            Esse dolore magna consequat sint tempor ipsum irure quis esse et
+            tempor ut sint ullamco. Occaecat mollit sit veniam ipsum nulla non
+            adipisicing enim dolor labore fugiat. Officia ullamco ad sit magna
+            non est nisi id sint id exercitation reprehenderit reprehenderit.
+            Excepteur est tempor laborum nostrud qui culpa qui dolore voluptate
+            velit duis. Dolor incididunt magna dolor pariatur. Nostrud minim
+            consequat officia tempor aliquip cillum officia. In velit officia et
+            aute ex pariatur velit duis ipsum. Cupidatat consectetur minim
+            commodo dolor mollit reprehenderit laboris. Occaecat eu ullamco
+            ullamco magna. Deserunt aliquip nulla in nisi. Occaecat minim
+            incididunt aliquip minim id eu adipisicing consectetur veniam quis.
+            Eu cillum eu laborum irure eiusmod amet officia Lorem elit laboris
+            nostrud amet. Quis labore do do amet esse voluptate. Nisi culpa
+            ullamco nostrud pariatur excepteur adipisicing voluptate
+            reprehenderit officia. Aliqua anim eu officia culpa exercitation
+            dolore reprehenderit irure est occaecat minim laboris reprehenderit.
+        </>
+    );
+};
+
+export const Default: Story = {
+    render: () => {
+        return (
+            <div class='flex size-96 flex-col gap-2 overflow-y-auto p-2'>
+                <h1 class='font-semibold text-xl'>Message</h1>
+                <ShowMore>
+                    <ShowMore.Content class='max-h-32 text-text-secondary'>
+                        <LoremIpsum />
+                    </ShowMore.Content>
+                    <ShowMore.Button />
+                </ShowMore>
+            </div>
+        );
+    },
+};
--- src/ui/ShowMore/ShowMore.tsx
+++ src/ui/ShowMore/ShowMore.tsx
@@ -1,7 +1,152 @@
-import type { Component } from 'solid-js';
+import {
+    type Accessor,
+    createContext,
+    createEffect,
+    createSignal,
+    type FlowComponent,
+    type JSX,
+    Match,
+    onCleanup,
+    onMount,
+    type Setter,
+    Switch,
+    useContext,
+} from 'solid-js';
 
-export type ShowMoreProps = {};
+import { Button } from '~/ui/Button';
+import { cn } from '~/utils';
 
-export const ShowMore: Component<ShowMoreProps> = (props) => {
-    return <div>ShowMore</div>;
+export const ShowMoreContext = createContext<{
+    isOverflowing: Accessor<boolean>;
+    setIsOverflowing: Setter<boolean>;
+    isExpanded: Accessor<boolean>;
+    setIsExpanded: Setter<boolean>;
+}>();
+
+export const useShowMoreContext = () => {
+    const context = useContext(ShowMoreContext);
+    if (!context) {
+        throw new Error(
+            'useShowMoreContext must be used within the ShowMoreContext provider',
+        );
+    }
+    return context;
 };
+
+export type ShowMoreProps = {
+    class?: string;
+    defaultExpanded?: boolean;
+    children: JSX.Element;
+};
+
+type ShowMoreCompound = {
+    (props: ShowMoreProps): JSX.Element;
+
+    Button: typeof ShowMoreButton;
+    Content: typeof ShowMoreContent;
+};
+
+export const ShowMore: ShowMoreCompound = (props) => {
+    const [isExpanded, setIsExpanded] = createSignal(
+        props.defaultExpanded ?? false,
+    );
+
+    const [isOverflowing, setIsOverflowing] = createSignal(false);
+
+    return (
+        <ShowMoreContext.Provider
+            value={{
+                isExpanded,
+                setIsExpanded,
+                isOverflowing,
+                setIsOverflowing,
+            }}
+        >
+            <div class={cn('flex flex-col gap-2', props.class)}>
+                {props.children}
+            </div>
+        </ShowMoreContext.Provider>
+    );
+};
+
+type ShowMoreContentProps = {
+    class?: string;
+};
+
+const ShowMoreContent: FlowComponent<ShowMoreContentProps> = (props) => {
+    const ctx = useShowMoreContext();
+
+    let contentRef!: HTMLDivElement;
+    let resizeObserver: ResizeObserver | undefined;
+
+    const measure = () => {
+        if (!contentRef) return;
+
+        const overflowing =
+            contentRef.scrollHeight > contentRef.clientHeight + 1;
+
+        ctx.setIsOverflowing(overflowing);
+    };
+
+    onMount(() => {
+        measure();
+
+        resizeObserver = new ResizeObserver(() => {
+            measure();
+        });
+
+        if (contentRef) {
+            resizeObserver.observe(contentRef);
+        }
+    });
+
+    createEffect(() => {
+        ctx.isExpanded();
+        queueMicrotask(measure);
+    });
+
+    onCleanup(() => {
+        resizeObserver?.disconnect();
+    });
+
+    return (
+        <div
+            class={cn(
+                'overflow-hidden',
+                props.class,
+                ctx.isExpanded() && 'max-h-none',
+            )}
+            ref={contentRef}
+        >
+            {props.children}
+        </div>
+    );
+};
+
+type ShowMoreButtonProps = {
+    class?: string;
+    children?: (isExpanded: boolean) => JSX.Element;
+};
+
+const ShowMoreButton = (props: ShowMoreButtonProps) => {
+    const ctx = useShowMoreContext();
+
+    return (
+        <Button
+            class={cn('justify-center', props.class)}
+            onClick={() => ctx.setIsExpanded((prev) => !prev)}
+            variant={ctx.isExpanded() ? 'outline' : 'secondary'}
+        >
+            <Switch>
+                <Match when={props.children}>
+                    {props.children?.(ctx.isExpanded())}
+                </Match>
+                <Match when={!ctx.isExpanded()}>Show More</Match>
+                <Match when={ctx.isExpanded()}>Show less</Match>
+            </Switch>
+        </Button>
+    );
+};
+
+ShowMore.Button = ShowMoreButton;
+ShowMore.Content = ShowMoreContent;
