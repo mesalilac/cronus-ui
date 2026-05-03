7258dc8f78bcb412d7afc3d8f1b4941332098bac
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-05-03 06:30:50 +0300

    feat(ui/Select): add `[scrollbar-gutter:stable]`

--- src/ui/Select/Content.tsx
+++ src/ui/Select/Content.tsx
@@ -64,7 +64,7 @@ export const Content = (rawProps: SelectContentProps) => {
         >
             <div
                 class={cn(
-                    'mt-1 mb-1 max-h-80 overflow-y-auto overscroll-contain rounded-default border border-border bg-surface-2 text-text-primary shadow-default',
+                    'mt-1 mb-1 max-h-80 overflow-y-auto overscroll-contain rounded-default border border-border bg-surface-2 text-text-primary shadow-default [scrollbar-gutter:stable]',
                     props.class,
                 )}
                 ref={divRef}
