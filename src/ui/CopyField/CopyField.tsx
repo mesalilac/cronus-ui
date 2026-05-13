7d19d29b79a5617d46ccdcd464121bd00d46939a
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-05-13 05:43:55 +0300

    Update CopyField.tsx

--- src/ui/CopyField/CopyField.tsx
+++ src/ui/CopyField/CopyField.tsx
@@ -1,7 +1,72 @@
-import type { Component } from 'solid-js';
+import {
+    type Component,
+    createSignal,
+    Show,
+    type VoidComponent,
+} from 'solid-js';
 
-export type CopyFieldProps = {};
+import { IconEditCopy, IconInterfaceCheck } from '~/icons';
+import { Button } from '~/ui/Button';
+import { Input } from '~/ui/Input';
+import { Textarea } from '~/ui/Textarea';
+import { cn } from '~/utils';
+
+export type CopyFieldProps = {
+    value?: string;
+    multiline?: boolean;
+    class?: string;
+};
+
+const CopyButton: VoidComponent<{
+    value: string | undefined;
+    multiline: boolean | undefined;
+}> = (props) => {
+    const [isCopied, setIsCopied] = createSignal(false);
+
+    const handleCopy = () => {
+        setIsCopied(true);
+
+        // TODO: Copy
+
+        setTimeout(() => {
+            setIsCopied(false);
+        }, 800);
+    };
+
+    return (
+        <Button
+            appearance={isCopied() ? 'success' : 'secondary'}
+            onClick={handleCopy}
+            variant={isCopied() ? 'soft' : 'solid'}
+        >
+            <Show
+                fallback={
+                    <>
+                        <IconEditCopy />
+                        <Show when={props.multiline}>Copy into clipboard</Show>
+                    </>
+                }
+                when={isCopied()}
+            >
+                <IconInterfaceCheck />
+                <Show when={props.multiline}>Copied!</Show>
+            </Show>
+        </Button>
+    );
+};
 
 export const CopyField: Component<CopyFieldProps> = (props) => {
-    return <div>CopyField</div>;
+    return (
+        <div
+            class={cn('flex gap-1', props.multiline && 'flex-col', props.class)}
+        >
+            <Show
+                fallback={<Input readOnly value={props.value ?? ''} />}
+                when={props.multiline}
+            >
+                <Textarea readOnly value={props.value ?? ''} />
+            </Show>
+            <CopyButton multiline={props.multiline} value={props.value} />
+        </div>
+    );
 };
