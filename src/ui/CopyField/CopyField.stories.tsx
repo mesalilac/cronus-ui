5136441fcc75dfcaa353c60ae4c30ffab4894b9d
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-05-13 07:07:09 +0300

    Update CopyField.stories.tsx

--- src/ui/CopyField/CopyField.stories.tsx
+++ src/ui/CopyField/CopyField.stories.tsx
@@ -34,20 +34,44 @@ const Wrapper: FlowComponent<{ class?: string }> = (props) => {
 
 export const Default: Story = {
     render: () => {
-        const value = 'hi';
+        const value =
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
 
-        return <CopyField value={value} />;
+        return (
+            <CopyField value={value}>
+                <CopyField.Input />
+                <CopyField.CopyButton />
+            </CopyField>
+        );
     },
 };
 
 export const Multiline: Story = {
     render: () => {
-        const value = 'hi';
+        const value =
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed\nsagittis, ipsum in tristique accumsan, erat sem bibendum justo, vel\ndictum arcu mi sit amet ex. Ut quis auctor massa, et porta felis\nDuis ut sapien lectus. Sed elementum\ncongue molestie. Donec aliquam\neros vel consectetur efficitur. Etiam commodo blandit neque, a\nfringilla magna tincidunt quis. Nulla sit amet odio sagittis,\nblandit nunc vel, scelerisque nulla. Nulla facilisis finibus\npretium. Nulla in nunc justo. Duis interdum pulvinar sem eu\nfermentum. Donec dictum a massa eu iaculis. Sed sagittis velit a\nnunc ultrices, eget tempus est tincidunt. Aenean interdum tellus ut\nfeugiat porttitor. Phasellus interdum consectetur elit eu dapibus.';
 
         return (
             <Wrapper>
-                <CopyField class='h-full' multiline value={value} />
+                <CopyField multiline value={value}>
+                    <CopyField.Input />
+                    <CopyField.CopyButton />
+                </CopyField>
             </Wrapper>
+        );
+    },
+};
+
+export const Editable: Story = {
+    render: () => {
+        const value =
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
+
+        return (
+            <CopyField readOnly={false} value={value}>
+                <CopyField.Input />
+                <CopyField.CopyButton />
+            </CopyField>
         );
     },
 };
