456fdb5fa65f21d98d2552f951ebab67947a9885
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-05-14 19:24:29 +0300

    feat(ui/Slider): use correct colors

--- src/ui/Slider/Slider.tsx
+++ src/ui/Slider/Slider.tsx
@@ -1,6 +1,7 @@
 import {
     type Accessor,
     createContext,
+    createMemo,
     createUniqueId,
     type JSX,
     useContext,
@@ -119,7 +120,42 @@ const SliderInput = (props: SliderInputProps) => {
 
     return (
         <input
-            class={cn('accent-accent', props.class)}
+            class={cn(
+                'h-4 w-full cursor-pointer appearance-none rounded-full bg-transparent',
+
+                // Webkit track
+                '[&::-webkit-slider-runnable-track]:h-2',
+                '[&::-webkit-slider-runnable-track]:rounded-full',
+                '[&::-webkit-slider-runnable-track]:bg-surface-3/30',
+
+                // Webkit thumb
+                '[&::-webkit-slider-thumb]:-mt-1.5',
+                '[&::-webkit-slider-thumb]:size-5',
+                '[&::-webkit-slider-thumb]:appearance-none',
+                '[&::-webkit-slider-thumb]:rounded-full',
+                '[&::-webkit-slider-thumb]:border',
+                '[&::-webkit-slider-thumb]:border-border',
+                '[&::-webkit-slider-thumb]:bg-accent',
+
+                // Firefox progress
+                '[&::-moz-range-progress]:h-2',
+                '[&::-moz-range-progress]:rounded-full',
+                '[&::-moz-range-progress]:bg-accent',
+
+                // Firefox track
+                '[&::-moz-range-track]:h-2',
+                '[&::-moz-range-track]:rounded-full',
+                '[&::-moz-range-track]:bg-surface-3/30',
+
+                // Firefox thumb
+                '[&::-moz-range-thumb]:size-5',
+                '[&::-moz-range-thumb]:rounded-full',
+                '[&::-moz-range-thumb]:border',
+                '[&::-moz-range-thumb]:border-border',
+                '[&::-moz-range-thumb]:bg-accent',
+
+                props.class,
+            )}
             disabled={ctx.disabled()}
             id={ctx.id()}
             list={ctx.list()}
