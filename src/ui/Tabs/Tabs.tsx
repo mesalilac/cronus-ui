3b41866553b78009a204fcfeef287621b1145b0f
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-05-16 03:55:43 +0300

    feat(ui/Tabs): setup Tabs

--- src/ui/Tabs/Tabs.tsx
+++ src/ui/Tabs/Tabs.tsx
@@ -1,7 +1,74 @@
-import type { Component } from 'solid-js';
+import {
+    type Accessor,
+    createContext,
+    type JSXElement,
+    mergeProps,
+    type ParentComponent,
+    useContext,
+} from 'solid-js';
 
-export type TabsProps = {};
+import type { PartialComponentProps } from '~/types';
+import { Button } from '~/ui/Button';
 
-export const Tabs: Component<TabsProps> = (props) => {
-    return <div>Tabs</div>;
+type TabsVariant = 'underline' | 'soft' | 'segmented';
+
+export type TabsProps = {
+    variant?: TabsVariant;
+    children: JSXElement;
 };
+
+export const TabsContext = createContext<{
+    variant: Accessor<TabsVariant>;
+}>();
+
+export const useTabsContext = () => {
+    const context = useContext(TabsContext);
+
+    if (!context) {
+        throw new Error(
+            'useTabsContext must be used within the TabsContext provider',
+        );
+    }
+
+    return context;
+};
+
+export const Tabs: TabsCompound = (rawProps) => {
+    const props = mergeProps(
+        { variant: 'underline' } satisfies PartialComponentProps<typeof Tabs>,
+        rawProps,
+    );
+
+    return (
+        <TabsContext.Provider
+            value={{
+                variant: () => props.variant,
+            }}
+        >
+            <div>{props.children}</div>
+        </TabsContext.Provider>
+    );
+};
+
+const Tab: ParentComponent = (props) => {
+    return <Button>{props.children}</Button>;
+};
+
+const List: ParentComponent = (props) => {
+    return <div>{props.children}</div>;
+};
+
+const Panel: ParentComponent = (props) => {
+    return <div>{props.children}</div>;
+};
+
+type TabsCompound = {
+    (props: TabsProps): JSXElement;
+    Tab: typeof Tab;
+    List: typeof List;
+    Panel: typeof Panel;
+};
+
+Tabs.Tab = Tab;
+Tabs.List = List;
+Tabs.Panel = Panel;
