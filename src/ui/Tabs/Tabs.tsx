9a224ea74815f07f13db5234a00ae7ef8f716cac
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-05-16 04:23:56 +0300

    feat(ui/Tabs): add more props

--- src/ui/Tabs/Tabs.tsx
+++ src/ui/Tabs/Tabs.tsx
@@ -1,24 +1,38 @@
 import {
     type Accessor,
+    createComputed,
     createContext,
+    createSignal,
     type JSXElement,
     mergeProps,
+    on,
     type ParentComponent,
     useContext,
 } from 'solid-js';
 
 import type { PartialComponentProps } from '~/types';
 import { Button } from '~/ui/Button';
+import { cn } from '~/utils';
 
 type TabsVariant = 'underline' | 'soft' | 'segmented';
+type TabsOrientation = 'horizontal' | 'vertical';
 
 export type TabsProps = {
+    value?: string;
+    defaultValue?: string;
+    onChange?: (value: string) => void;
+    orientation?: TabsOrientation;
     variant?: TabsVariant;
+    disabled?: boolean;
     children: JSXElement;
 };
 
 export const TabsContext = createContext<{
+    value: Accessor<string | undefined>;
+    onChange: (value: string) => void;
+    orientation: Accessor<TabsOrientation>;
     variant: Accessor<TabsVariant>;
+    disabled: Accessor<boolean | undefined>;
 }>();
 
 export const useTabsContext = () => {
@@ -35,14 +49,40 @@ export const useTabsContext = () => {
 
 export const Tabs: TabsCompound = (rawProps) => {
     const props = mergeProps(
-        { variant: 'underline' } satisfies PartialComponentProps<typeof Tabs>,
+        {
+            variant: 'underline',
+            orientation: 'horizontal',
+        } satisfies PartialComponentProps<typeof Tabs>,
         rawProps,
     );
+
+    const [value, setValue] = createSignal<string | undefined>(
+        props.value ?? props.defaultValue,
+    );
+
+    createComputed(
+        on(
+            () => props.value,
+            (val) => {
+                setValue(val);
+            },
+            { defer: true },
+        ),
+    );
+
+    const onChange = (val: string) => {
+        if (props.value === undefined) setValue(val);
+        props.onChange?.(val);
+    };
 
     return (
         <TabsContext.Provider
             value={{
+                value,
+                onChange,
+                orientation: () => props.orientation,
                 variant: () => props.variant,
+                disabled: () => props.disabled,
             }}
         >
             <div>{props.children}</div>
@@ -50,15 +90,23 @@ export const Tabs: TabsCompound = (rawProps) => {
     );
 };
 
-const Tab: ParentComponent = (props) => {
-    return <Button>{props.children}</Button>;
+const Tab: ParentComponent<{
+    class?: string;
+    value: string;
+    disabled?: boolean;
+}> = (props) => {
+    return (
+        <Button class={cn('capitalize', props.class)} disabled={props.disabled}>
+            {props.children ?? props.value}
+        </Button>
+    );
 };
 
-const List: ParentComponent = (props) => {
+const List: ParentComponent<{ class?: string }> = (props) => {
     return <div>{props.children}</div>;
 };
 
-const Panel: ParentComponent = (props) => {
+const Panel: ParentComponent<{ class?: string; value: string }> = (props) => {
     return <div>{props.children}</div>;
 };
 
