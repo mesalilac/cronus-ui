34e8b481f54a7ff3226b16858bf5543b9c7b1bf9
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-05-17 23:17:12 +0300

    feat(ui/Tooltip): add context

--- src/ui/Tooltip/Tooltip.tsx
+++ src/ui/Tooltip/Tooltip.tsx
@@ -1,11 +1,76 @@
-import type { JSXElement, ParentComponent } from 'solid-js';
+import {
+    type Accessor,
+    createComputed,
+    createContext,
+    createSignal,
+    type JSXElement,
+    on,
+    type ParentComponent,
+    type Setter,
+    useContext,
+} from 'solid-js';
 
 export type TooltipProps = {
+    open?: boolean;
+    defaultOpen?: boolean;
+    onOpenChange?: (open: boolean) => void;
+    openDelayMs?: number;
+    closeDelayMs?: number;
     children: JSXElement;
 };
 
+export const TooltipContext = createContext<{
+    isOpen: Accessor<boolean>;
+    setIsOpen: (open: boolean) => void;
+
+    openDelayMs: Accessor<number | undefined>;
+    closeDelayMs: Accessor<number | undefined>;
+}>();
+
+export const useTooltipContext = () => {
+    const context = useContext(TooltipContext);
+
+    if (!context) {
+        throw new Error(
+            'useTooltipContext must be used within the TooltipContext provider',
+        );
+    }
+
+    return context;
+};
+
 export const Tooltip: TooltipCompound = (props) => {
-    return <div>{props.children}</div>;
+    const [isOpen, setInternalIsOpen] = createSignal(
+        props.open ?? props.defaultOpen ?? false,
+    );
+
+    createComputed(
+        on(
+            () => Boolean(props.open),
+            (open) => {
+                setInternalIsOpen(open);
+            },
+            { defer: true },
+        ),
+    );
+
+    const setIsOpen = (open: boolean) => {
+        if (props.open === undefined) setInternalIsOpen(open);
+        props.onOpenChange?.(open);
+    };
+
+    return (
+        <TooltipContext.Provider
+            value={{
+                isOpen,
+                setIsOpen,
+                openDelayMs: () => props.openDelayMs,
+                closeDelayMs: () => props.closeDelayMs,
+            }}
+        >
+            {props.children}
+        </TooltipContext.Provider>
+    );
 };
 
 export const Trigger: ParentComponent<{ class?: string }> = (props) => {
