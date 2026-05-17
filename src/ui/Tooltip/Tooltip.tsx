654c5645032f6ba9fcfeb88d02703e680a7b969f
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-05-17 23:00:38 +0300

    feat(ui/Tooltip): add sub components

--- src/ui/Tooltip/Tooltip.tsx
+++ src/ui/Tooltip/Tooltip.tsx
@@ -1,7 +1,26 @@
-import type { Component } from 'solid-js';
+import type { JSXElement, ParentComponent } from 'solid-js';
 
-export type TooltipProps = {};
+export type TooltipProps = {
+    children: JSXElement;
+};
 
-export const Tooltip: Component<TooltipProps> = (props) => {
+export const Tooltip: TooltipCompound = (props) => {
+    return <div>{props.children}</div>;
+};
+
+export const Trigger: ParentComponent<{ class?: string }> = (props) => {
     return <div>Tooltip</div>;
 };
+
+export const Content: ParentComponent<{ class?: string }> = (props) => {
+    return <div>{props.children}</div>;
+};
+
+type TooltipCompound = {
+    (props: TooltipProps): JSXElement;
+    Trigger: typeof Trigger;
+    Content: typeof Content;
+};
+
+Tooltip.Trigger = Trigger;
+Tooltip.Content = Content;
