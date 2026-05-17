8710ac1a8d30b6063abcb4d844668ac047b186d2
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-05-17 05:14:14 +0300

    feat(ui/StatusDot): init

---
+++ src/ui/StatusDot/StatusDot.tsx
@@ -0,0 +1,7 @@
+import type { Component } from 'solid-js';
+
+export type StatusDotProps = {};
+
+export const StatusDot: Component<StatusDotProps> = (props) => {
+    return <div>StatusDot</div>;
+};
