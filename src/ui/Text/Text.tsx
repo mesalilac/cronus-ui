fda4e1eef1db10967cfc8eeec9cb6f5a3788e43e
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-05-23 20:44:25 +0300

    feat(ui/Text): write comp

--- src/ui/Text/Text.tsx
+++ src/ui/Text/Text.tsx
@@ -1,7 +1,59 @@
-import type { Component } from 'solid-js';
+import {
+    type JSXElement,
+    mergeProps,
+    splitProps,
+    type ValidComponent,
+} from 'solid-js';
 
-export type TextProps = {};
+import { Polymorphic, type PolymorphicProps } from '~/polymorphic';
+import { cn } from '~/utils';
 
-export const Text: Component<TextProps> = (props) => {
-    return <div>Text</div>;
+export type TextVariant =
+    | 'default'
+    | 'accent'
+    | 'primary'
+    | 'secondary'
+    | 'muted'
+    | 'info'
+    | 'success'
+    | 'warning'
+    | 'danger';
+
+const TEXT_VARIANT_STYLES: Record<TextVariant, string> = {
+    default: '',
+    accent: cn('text-accent'),
+    primary: cn('text-text-primary'),
+    secondary: cn('text-text-secondary'),
+    muted: cn('text-text-muted'),
+    info: cn('text-info'),
+    success: cn('text-text-success'),
+    warning: cn('text-text-warning'),
+    danger: cn('text-text-danger'),
+};
+
+export type TextProps = {
+    variant?: TextVariant;
+    class?: string;
+    children?: JSXElement;
+};
+
+export const Text = <T extends ValidComponent = 'span'>(
+    rawProps: PolymorphicProps<T, TextProps>,
+) => {
+    const props = mergeProps(
+        {
+            variant: 'default',
+        } satisfies Partial<TextProps>,
+        rawProps,
+    );
+
+    const [local, others] = splitProps(props, ['variant', 'class']);
+
+    return (
+        <Polymorphic
+            as='span'
+            class={cn(TEXT_VARIANT_STYLES[local.variant], local.class)}
+            {...others}
+        />
+    );
 };
