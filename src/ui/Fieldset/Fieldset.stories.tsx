750a0aa04be3932d8084aab1fcc5526f6c65b5e7
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-05-24 22:34:53 +0300

    Update Fieldset.stories.tsx

--- src/ui/Fieldset/Fieldset.stories.tsx
+++ src/ui/Fieldset/Fieldset.stories.tsx
@@ -41,3 +41,76 @@ export const Default: Story = {
         );
     },
 };
+
+export const LegendStart: Story = {
+    render: () => {
+        return (
+            <div class='size-96'>
+                <Fieldset>
+                    <Fieldset.Legend placement='start'>
+                        Shipping details
+                    </Fieldset.Legend>
+                    <Fieldset.Description>
+                        Without this your odds of getting your order are low.
+                    </Fieldset.Description>
+                    <Field>
+                        <Field.Label>Full name</Field.Label>
+                        <Input value='' />
+                    </Field>
+                    <Field>
+                        <Field.Label>Street address</Field.Label>
+                        <Input value='' />
+                    </Field>
+                </Fieldset>
+            </div>
+        );
+    },
+};
+export const LegendCenter: Story = {
+    render: () => {
+        return (
+            <div class='size-96'>
+                <Fieldset>
+                    <Fieldset.Legend placement='center'>
+                        Shipping details
+                    </Fieldset.Legend>
+                    <Fieldset.Description>
+                        Without this your odds of getting your order are low.
+                    </Fieldset.Description>
+                    <Field>
+                        <Field.Label>Full name</Field.Label>
+                        <Input value='' />
+                    </Field>
+                    <Field>
+                        <Field.Label>Street address</Field.Label>
+                        <Input value='' />
+                    </Field>
+                </Fieldset>
+            </div>
+        );
+    },
+};
+export const LegendEnd: Story = {
+    render: () => {
+        return (
+            <div class='size-96'>
+                <Fieldset>
+                    <Fieldset.Legend placement='end'>
+                        Shipping details
+                    </Fieldset.Legend>
+                    <Fieldset.Description>
+                        Without this your odds of getting your order are low.
+                    </Fieldset.Description>
+                    <Field>
+                        <Field.Label>Full name</Field.Label>
+                        <Input value='' />
+                    </Field>
+                    <Field>
+                        <Field.Label>Street address</Field.Label>
+                        <Input value='' />
+                    </Field>
+                </Fieldset>
+            </div>
+        );
+    },
+};
