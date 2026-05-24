b7b46d8260ac751f184072d7f0211ff15645d500
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-05-24 22:33:39 +0300

    Create Fieldset.stories.tsx

---
+++ src/ui/Fieldset/Fieldset.stories.tsx
@@ -0,0 +1,43 @@
+import type { Meta, StoryObj } from 'storybook-solidjs-vite';
+
+import { Field } from '~/ui/Field';
+import { Input } from '~/ui/Input';
+
+import { Fieldset } from './Fieldset';
+
+type StoryComponent = typeof Fieldset;
+
+const meta = {
+    title: 'Ui/Fieldset',
+    component: Fieldset,
+    parameters: {
+        layout: 'centered',
+    },
+} satisfies Meta<StoryComponent>;
+
+export default meta;
+
+type Story = StoryObj<StoryComponent>;
+
+export const Default: Story = {
+    render: () => {
+        return (
+            <div class='size-96'>
+                <Fieldset>
+                    <Fieldset.Legend>Shipping details</Fieldset.Legend>
+                    <Fieldset.Description>
+                        Without this your odds of getting your order are low.
+                    </Fieldset.Description>
+                    <Field>
+                        <Field.Label>Full name</Field.Label>
+                        <Input value='' />
+                    </Field>
+                    <Field>
+                        <Field.Label>Street address</Field.Label>
+                        <Input value='' />
+                    </Field>
+                </Fieldset>
+            </div>
+        );
+    },
+};
