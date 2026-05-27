8ac9210a26925581717bbb806240456c2db68e3b
Abdalrzag Eisa <53495583+mesalilac@users.noreply.github.com>
2026-05-27 07:12:06 +0300

    feat(ui/Select): autofocus searchbar

--- src/ui/Select/Searchbar.tsx
+++ src/ui/Select/Searchbar.tsx
@@ -28,6 +28,7 @@ export const Searchbar = (props: SelectSearchbarProps) => {
     return (
         <div class='flex flex-1 items-center gap-1'>
             <Input
+                autoFocus
                 class={cn('px-2 py-1', props.class)}
                 onInput={props.setQuery}
                 type='search'
